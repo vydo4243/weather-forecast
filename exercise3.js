8684d3d5018c4159441ab5696ee678422169abde
tuongvy <22521701@gm.uit.edu.vn>
2026-04-05 18:16:39 +0700

    Weather Object

---
+++ exercise3.js
@@ -0,0 +1,7 @@
+const currentWeather = {
+  temperature: 75,
+  conditions: "sunny",
+};
+
+console.log(currentWeather.temperature);
+console.log(`It's ${currentWeather.temperature} degrees F and ${currentWeather.conditions}.`);
