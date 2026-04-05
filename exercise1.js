cfbf22786a97de49c87725bb60857d4b254f6df3
tuongvy <22521701@gm.uit.edu.vn>
2026-04-05 18:11:15 +0700

    Today's weather

---
+++ exercise1.js
@@ -0,0 +1,13 @@
+const temperature = 75;
+const conditions = "sunny";
+const sunriseTime = "6:05 AM";
+const sunsetTime = "5:45 PM";
+
+console.log(temperature);
+console.log(conditions);
+console.log(sunriseTime);
+console.log(sunsetTime);
+
+const weatherSummary = `It's currently ${conditions} and ${temperature} degrees. The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`;
+
+console.log(weatherSummary);
